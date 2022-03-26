50f1ea8ed410d6dc056df051b53acec75a4779a0
jacksonsr451 <jackson.severino.rocha@escola.pr.gov.br>
2022-03-26 19:41:40 -0300

    Refactoring: return by presenter in delete_user_presenter.py

--- src/interface/presenters/delete_user_presenter.py
+++ src/interface/presenters/delete_user_presenter.py
@@ -4,9 +4,12 @@ from src.applications.usecases.delete_user_by_id.delete_user_by_id_presenter_int
 class DeleteUserByIDByIDPresenter(DeleteUserByIDPresenterInterface):
     def __init__(self):
         self.value: bool = False
+        self.code = 409
 
     def present(self, value: bool):
         self.value = value
 
     def get_view_model(self):
-        return self.value
+        if self.value:
+            self.code = 200
+        return {"result": self.value}, self.code
